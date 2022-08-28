c4d1a9e6a13b41cb6c385a61e7352c8ada471ef7
Malonejay5 <103909037+Malonejay5@users.noreply.github.com>
2022-08-28 01:03:30 -0700

    Update sku.py

--- sku.py
+++ sku.py
@@ -7,33 +7,31 @@ import os
 import time
 
 
-newSku_input = input("Add a New Sku? [y / n]: ")
 
-if newSku_input == "n":
-    row = 1
-    col = 0
-    with open("skulist.json") as list: #### READS SKU LIST
-        skusdata = json.load(list)
-        skus = skusdata['skus']
+row = 1
+col = 0
+with open("skulist.json") as list: #### READS SKU LIST
+    skusdata = json.load(list)
+    skus = skusdata['skus']
 
 
-    workbook = xwrite.Workbook('Sku_List.xlsx') ##### CREATES EXCEL FILE
-    worksheet = workbook.add_worksheet()
-    for sku in skus: #### GOES THROUGH SKU LIST
+workbook = xwrite.Workbook('Sku_List.xlsx') ##### CREATES EXCEL FILE
+worksheet = workbook.add_worksheet()
+for sku in skus: #### GOES THROUGH SKU LIST
 
 
-        imageURL = (f"https://images.footlocker.com/is/image/EBFL2/{sku}?wid=60&hei=60&fmt=png-alpha")
+    imageURL = (f"https://images.footlocker.com/is/image/EBFL2/{sku}?wid=60&hei=60&fmt=png-alpha")
 
-        r = requests.get(imageURL)
-        img = Image.open(BytesIO(r.content))
-        imgSave = img.save(f"SKU: {sku}.png") #### PULLS IMAGE FROM URL
+    r = requests.get(imageURL)
+    img = Image.open(BytesIO(r.content))
+    imgSave = img.save(f"SKU: {sku}.png") #### PULLS IMAGE FROM URL
 
-        worksheet.insert_image(row - 1, col + 2,  f"SKU: {sku}.png") 
-        worksheet.write(row, col, sku)
-        print(f'{sku} : Done', end= '\n')
-        row += 2 # GOES DOWN TWO ROWS
+    worksheet.insert_image(row - 1, col + 2,  f"SKU: {sku}.png") 
+    worksheet.write(row, col, sku)
+    print(f'{sku} : Done', end= '\n')
+    row += 2 # GOES DOWN TWO ROWS
 
-        
-    workbook.close() #### WRITES EXCEL FILE
+
+workbook.close() #### WRITES EXCEL FILE
 
 
