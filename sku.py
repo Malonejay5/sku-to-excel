77dc0b449fe7c0dc4e294165a1f0fa4e59bee8a2
Malonejay5 <103909037+Malonejay5@users.noreply.github.com>
2022-08-28 01:02:23 -0700

    Update sku.py

--- sku.py
+++ sku.py
@@ -35,32 +35,5 @@ if newSku_input == "n":
 
         
     workbook.close() #### WRITES EXCEL FILE
-# remove = input("Remove Images? [y / n]: ")
-# if remove == 'y':
-#     time.sleep(5)
-#     print(f"Sleeping for {5} Seconds...")
-#     for sku in skus:
-#         os.remove(f"SKU: {sku}.png")
 
-if newSku_input == "y":
-    with open("skulist.json") as list:
-        skusdata = json.load(list)
-        skus = skusdata['skus']
-        #print(skus)
-
-    workbook = xwrite.Workbook('test_excel.xlsx')
-    worksheet = workbook.add_worksheet()
-    new_sku = input("Enter Sku: ")
-    imageURL = (f"https://images.footlocker.com/is/image/EBFL2/{new_sku}?wid=60&hei=60&fmt=png-alpha")
-
-    r = requests.get(imageURL)
-    img = Image.open(BytesIO(r.content))
-    imgSave = img.save(f"test{new_sku}.png")
-
-    row = len(skus)*2
-    col = len(skus)*2
-    worksheet.insert_image(row + 3, col + 4,  f"test{new_sku}.png")
-    worksheet.write(row + 1, col + 1, new_sku)
-    print(f"G{row}", f'test{new_sku}.png')
-    print(row)
 
